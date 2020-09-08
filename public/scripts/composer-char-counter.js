1005a80e5ce83f04333e1a9b70a4d97850e7b072
DevHalpin <echidnablade@hotmail.com>
2020-09-08 14:53:16 -0700

    created basic layout of page

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(() => {
+  $("#tweet-text").on('input', function() {
+    const len = $(this).val().length
+    $("#tweet-text, .counter").text(140-len)
+    if (len > 140) {
+      $("#tweet-text, .counter").addClass("overlimit")
+    } else {
+      if ($("#tweet-text, .counter").hasClass("overlimit")) {
+        $("#tweet-text, .counter").removeClass("overlimit")
+      }
+    }
+  })
+})
