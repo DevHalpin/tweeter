9a36bf3c841055b2e0e6799d1f3fd51b758d197a
DevHalpin <echidnablade@hotmail.com>
2020-09-10 16:34:27 -0700

    Code clean up and comments added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,99 +1,110 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-$(document).ready(() => {
 
-  const sanitize = (string) => {
-    const map = {
-        '&': '&amp;',
-        '<': '&lt;',
-        '>': '&gt;',
-        '"': '&quot;',
-        "'": '&#x27;',
-        "/": '&#x2F;',
+  /*
+  * Sanitize function to prevent XSS
+  * Takes in a string and returns sanitized string
+  */
+ const sanitize = (string) => {
+   const map = {
+     '&': '&amp;',
+     '<': '&lt;',
+     '>': '&gt;',
+     '"': '&quot;',
+     "'": '&#x27;',
+     '/': '&#x2F;',
     };
     const reg = /[&<>"'/]/ig;
-    return string.replace(reg, (match)=>(map[match]));
-  }
-
-  const createTweetElement = (tweetData) => {
-
-    let article = $("<article>").addClass("tweet");
-    const date = moment(tweetData.created_at).fromNow();
-
-    article.append(`
-    <header>
-      <span class="h-image">
-        <img src=${tweetData.user.avatars}>
-      </span>
-      <span class="h-name">
-        <h3>${tweetData.user.name}</h3>
-      </span>
-      <span class="handle">
-        <h3>${tweetData.user.handle}</h3>
-      </span>
-    </header>
-    <p>
-      ${sanitize(tweetData.content.text)}
-    </p>
-    <footer class="foot">
-      <span class="footer-left">
-        <h6>${date}</h6>
-      </span>
-      <span class="footer-right">
-          <i class='fas fa-flag' style='font-size:24px'></i>
-          <i class='fas fa-retweet' style='font-size:24px'></i>
-          <i class='fas fa-heart' style='font-size:24px'></i>
-      </span>
-    </footer>`);
-    return article;
+    return string.replace(reg, match => map[match]);
   };
-
-  const renderTweets = function(tweets) {
-    $("#tweet-container").empty();
-    for (tweet of tweets) {
-      $('#tweet-container').prepend(createTweetElement(tweet));
+  
+  /*
+  * createTweetElement takes in a data object with tweet info
+  * Returns the completed tweet article element
+  */
+ const createTweetElement = (tweetData) => {
+   
+   let article = $('<article>').addClass('tweet');
+   const date = moment(tweetData.created_at).fromNow();
+   
+   article.append(`
+   <header>
+   <span class='h-image'>
+   <img src=${tweetData.user.avatars}>
+   </span>
+   <span class='h-name'>
+   <h3>${tweetData.user.name}</h3>
+   </span>
+   <span class='handle'>
+   <h3>${tweetData.user.handle}</h3>
+   </span>
+   </header>
+   <p>
+   ${sanitize(tweetData.content.text)}
+   </p>
+   <footer class='foot'>
+   <span class='footer-left'>
+   <h6>${date}</h6>
+   </span>
+   <span class='footer-right'>
+   <i class='fas fa-flag' style='font-size:24px'></i>
+   <i class='fas fa-retweet' style='font-size:24px'></i>
+   <i class='fas fa-heart' style='font-size:24px'></i>
+   </span>
+   </footer>`);
+   return article;
+  };
+  
+  /*
+  * renderTweets takes in an array of tweets
+  * and prepends each one into the tweet container
+  */
+ const renderTweets = function(tweets) {
+   $('#tweet-container').empty();
+   for (const tweet of tweets) {
+     $('#tweet-container').prepend(createTweetElement(tweet));
     }
   };
+  
+  /*
+  * loadTweets pulls tweets from database 
+  * and pushes them to renderTweets
+  */
+ const loadTweets = function() {
+   $.get('/tweets',renderTweets);
+  };
 
-  const loadTweets = function() {
-    $.get("/tweets",renderTweets);
-  }
+$(document).ready(() => {
+  loadTweets();
+  $('.error').hide();
+  $('.new-tweet').hide();
 
-  loadTweets(); 
-  $(".error").hide();
-  $(".new-tweet").hide();
-
-  $(".write-tweet").on('click', function(e) {
-    $(".new-tweet").slideToggle();
-    $("#tweet-text").val('').focus();
-    $("#tweet-test, .counter").text(140);
-  })
+  $('.write-tweet').on('click', function() {
+    $('.new-tweet').slideToggle();
+    $('#tweet-text').val('').focus();
+    $('#tweet-test, .counter').text(140);
+  });
     
-  $(".create-tweet").on('submit', function (e) {
-    e.preventDefault(); 
+  $('.create-tweet').on('submit', function(e) {
+    e.preventDefault();
     $('.error-message').text('');
-    $(".error").slideUp();
-    const text = $("#tweet-text").val();
+    $('.error').slideUp();
+    const text = $('#tweet-text').val();
     if (text === '' || text === null) {
-      $(".error").slideDown();
-      $(".error-message").text("🔴 Tweet text field is empty! 🔴");
+      $('.error').slideDown();
+      $('.error-message').text('🔴 Tweet text field is empty! 🔴');
     } else if (text.length > 140) {
-      $(".error").slideDown();
-      $(".error-message").text("🔴 Tweet text is longer than 140 characters! 🔴");
+      $('.error').slideDown();
+      $('.error-message').text('🔴 Tweet text is longer than 140 characters! 🔴');
     } else {
       $.ajax({
-        type: "POST",
-        url: "/tweets/",
-        data: $(".create-tweet").serialize(),
+        type: 'POST',
+        url: '/tweets/',
+        data: $('.create-tweet').serialize(),
       })
-      .done(function(data) {
+      .done(function() {
         loadTweets();
-        $("#tweet-text").val('');
-        $("#tweet-test, .counter").text(140);
-      })
+        $('#tweet-text').val('');
+        $('#tweet-test, .counter').text(140);
+      });
     }
   });
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,16 @@
+/* creates the logic to count down as characters are typed
+ * and turns counter red when 140 chars is passed
+ */
 $(document).ready(() => {
-  $("#tweet-text").on('input', function() {
-    const len = $(this).val().length
-    $("#tweet-text, .counter").text(140-len)
+  $('#tweet-text').on('input', function() {
+    const len = $(this).val().length;
+    $('#tweet-text, .counter').text(140 - len);
     if (len > 140) {
-      $("#tweet-text, .counter").addClass("overlimit")
+      $('#tweet-text, .counter').addClass('overlimit');
     } else {
-      if ($("#tweet-text, .counter").hasClass("overlimit")) {
-        $("#tweet-text, .counter").removeClass("overlimit")
+      if ($('#tweet-text, .counter').hasClass('overlimit')) {
+        $('#tweet-text, .counter').removeClass('overlimit');
       }
     }
-  })
-})
+  });
+});
